09d90c0a7fa753c2fd014299e14548f92e39a195
Xiangxin Zheng <pandazxx@gmail.com>
2012-11-05 22:50:33 +0800

    - Added close button - Added copy button, will show links with alert

--- emuleBookmarklet.js
+++ emuleBookmarklet.js
@@ -1,16 +1,20 @@
 javascript:(function(){
+/*
     var y = document.createElement('SCRIPT');
     y.type = 'text/javascript';
     y.src = 'http://mootools.net/download/get/mootools-core-1.4.5-full-compat-yc.js';
     document.getElementsByTagName('head')[0].appendChild(y);
+    */
     var x = document.createElement('SCRIPT');
     x.type = 'text/javascript';
     x.src = 'http://code.jquery.com/jquery-1.8.2.min.js';
     document.getElementsByTagName('head')[0].appendChild(x);
+    /*
     y = document.createElement('SCRIPT');
     y.type = 'text/javascript';
     y.src = 'https://raw.github.com/pandazxx/allDownLinkBookmarkLet/master/EnybyClipboard.js';
     document.getElementsByTagName('head')[0].appendChild(y);
+    */
 
     var d = jQuery('<div id="emuleBookmarkLetDiv" style="border: 6px solid rgb(180, 180, 180); margin: 0px; padding: 0px; position: fixed; background-color: white;OVERFLOW-Y:scroll; z-index: 999; width: 500px; top: 6px; right: 6px;"></div>');
     /*d.prop('style', 'border: 6px solid rgb(180, 180, 180); margin: 0px; padding: 0px; position: fixed; background-color: white; z-index: 0; width: 500px; height: 370px;  top: 6px; right: 6px;');*/
@@ -32,9 +36,11 @@ javascript:(function(){
             clipText += this.href + "\n";
         }
     });
+    /*
     var enyby_clip = new EnybyClipboard();
 
     enyby_clip.copyText(clipText);
+    */
     var selectAll = jQuery('<input type="checkbox">');
     selectAll.change(function() {
         if (this.checked) {
@@ -50,6 +56,22 @@ javascript:(function(){
         }
     });
     d.append(selectAll);
+    d.append("<span>SelectAll</span><br/>");
+    var copyBtn = jQuery('<input type="button" value="Copy">');
+    copyBtn.click(function() {
+        var content = "";
+        d.find('input.emulelink:checked').each(function() {
+            content += $(this).val() + "<br/>";
+        });
+        alert(content);
+    });
+
+    d.append(copyBtn);
+
+    jQuery('<input type="button" value="close">').click(function() {
+        d.remove();
+    }).appendTo(d);
+
     d.append('<br/><br/>');
 
 
